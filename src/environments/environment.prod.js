db0027c266b4e99f076f6d20d8cc5e5cca6ee93c
kemu <kemu430113@gmail.com>
2023-09-18 12:56:12 +0900

    fix router

--- src/environments/environment.prod.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.environment = void 0;
-exports.environment = {
-  production: true,
-  googleAdClient: 'ca-pub-6219032039771696',
-  adsenseProvider: 'google',
-};
-//# sourceMappingURL=environment.prod.js.map
